64d6d735bf9f4dd531e651e1e437f1fd44d675f2
arissetyawan1 <aris.setyawan354313@gmail.com>
2022-09-24 02:34:19 +0700

    fix : repository

--- repository/customer.js
+++ repository/customer.js
@@ -5,8 +5,6 @@ const fetcher = (url) => fetch(url).then(res => res.json());
 const url = {
     getList: (page) => `https://gorest.co.in/public/v1/users?page=${page}` ,
     getListDetail: (id) => `https://gorest.co.in/public/v1/users/${id}`,
-    profileDetailCards: (id) => `/profile/${id}/cards`,
-    updatePhoneNumber: () => `/users/update-phone-number`
 };
 
 const hooks = {
@@ -16,9 +14,6 @@ const hooks = {
     useGetProfileDetail(id) {
         return useSWR(url.getListDetail(id), fetcher);
     },
-    useGetProfileDetailCards(id) {
-        return useSWR(url.profileDetailCards(id), http.fetcher);
-    }
 }
 
 const api = {
